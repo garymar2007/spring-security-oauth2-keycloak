39f5f9901dbf8d1de5c5c91eed56b6553567cb90
Gary Ma <gary.ma@csgi.com>
2023-04-23 13:41:20 +0200

    initial commit - oauth2-authorization-code

---
+++ src/main/java/com/gary/springsecurityoauth2keycloakdemo/SpringSecurityOauth2KeycloakDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gary.springsecurityoauth2keycloakdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2KeycloakDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2KeycloakDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gary/springsecurityoauth2keycloakdemo/SpringSecurityOauth2KeycloakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.springsecurityoauth2keycloakdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2KeycloakDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
