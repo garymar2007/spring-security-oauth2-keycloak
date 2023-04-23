6feee073123aa86b9864b9219dda569abd3eb429
Gary Ma <gary.ma@csgi.com>
2023-04-23 13:56:55 +0200

    initially adding oauth2-pkce code

---
+++ oauth2-pkce-demo/src/test/java/com/gary/oauth2pkcedemo/Oauth2PkceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.oauth2pkcedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PkceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
